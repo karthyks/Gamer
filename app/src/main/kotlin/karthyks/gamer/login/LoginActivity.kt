6ea21e0925b54051c4259e40a3971e6ec4f83ded
Karthik Periasami <karthikperiasami@Karthiks-MacBook-Pro.local>
2019-04-20 19:09:25 +0530

    add launcher icons and login buttons

---
+++ app/src/main/kotlin/karthyks/gamer/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package karthyks.gamer.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.karthyks.gamer.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/kotlin/karthyks/gamer/splash/SplashActivity.kt
@@ -0,0 +1,9 @@
+package karthyks.gamer.splash
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+
+
+}
